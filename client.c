42a68028fae72faf79faf2986ac281b00e223a47
aelda <kate1011good@gmail.com>
2014-11-15 16:11:10 +0800

    modify server and client

--- client.c
+++ client.c
@@ -5,31 +5,44 @@
 #include <stdio.h>
 int main()
 {
-    int sockfd;
-    struct sockaddr_in dest;
-    char buffer[128];
-    char resp[10]="clientack";
+	int sockfd;
+	struct sockaddr_in dest;
+	char buffer[] = "Hello World!";
 
-    /* create socket */
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	/* create socket , same as client */
+	sockfd = socket(PF_INET, SOCK_STREAM, 0);
 
-    /* initialize value in dest */
-    bzero(&dest, sizeof(dest));
-    dest.sin_family = PF_INET;
-    dest.sin_port = htons(8889);
-    dest.sin_addr.s_addr = inet_addr("127.0.0.1");
-    //inet_aton("127.0.0.1", &dest.sin_addr);
+	/* initialize structure dest */
+	bzero(&dest, sizeof(dest));
+	dest.sin_family = PF_INET;
+	dest.sin_port = htons(8889);
+	/* this line is different from client */
+	dest.sin_addr.s_addr = INADDR_ANY;
 
-    /* Connecting to server */
-    connect(sockfd, (struct sockaddr*)&dest, sizeof(dest));
+	/* Assign a port number to socket */
+	bind(sockfd, (struct sockaddr*)&dest, sizeof(dest));
 
-    /* Receive message from the server and print to screen */
-    bzero(buffer, 128);
-    recv(sockfd, buffer, sizeof(buffer), 0);
-    printf("receive from server: %s\n", buffer);
-    send(sockfd,resp,sizeof(resp),0);
-    /* Close connection */
-    close(sockfd);
+	/* make it listen to socket with max 20 connections */
+	listen(sockfd, 20);
 
-    return 0;
+	/* infinity loop -- accepting connection from client forever */
+	while(1)
+	{
+		int clientfd;
+		struct sockaddr_in client_addr;
+		int addrlen = sizeof(client_addr);
+
+		/* Wait and Accept connection */
+		clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+
+		/* Send message */
+		send(clientfd, buffer, sizeof(buffer), 0);
+
+		/* close(client) */
+		close(clientfd);
+	}
+
+	/* close(server) , but never get here because of the loop */
+	close(sockfd);
+	return 0;
 }
--- server.c
+++ server.c
@@ -7,46 +7,29 @@
 #include <stdio.h>
 int main()
 {
-    int sockfd;
-    struct sockaddr_in dest;
-    char buffer[20] = "Hi client! ";
-    char buffer1[10];
-    /* create sockett */
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	int sockfd;
+	struct sockaddr_in dest;
+	char buffer[128];
 
-    /* initialize structure dest */
-    bzero(&dest, sizeof(dest));
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(8889);
-    /* this line is different from client */
-    dest.sin_addr.s_addr = INADDR_ANY;
+	/* create socket */
+	sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	
+	/* initialize value in dest */
+	bzero(&dest, sizeof(dest));
+	dest.sin_family = PF_INET;
+	dest.sin_port = htons(8889);
+	inet_aton("127.0.0.1", &dest.sin_addr);
 
-    /* Assign a port number to socket */
-    bind(sockfd, (struct sockaddr*)&dest, sizeof(dest));
+	/* Connecting to server */
+	connect(sockfd, (struct sockaddr*)&dest, sizeof(dest));
+	
+	/* Receive message from the server and print to screen */
+	bzero(buffer, 128);
+	recv(sockfd, buffer, sizeof(buffer), 0);
+	printf("%s", buffer);
 
-    /* make it listen to socket with max 20 connections */
-    listen(sockfd, 20);
+	/* Close connection */
+	close(sockfd);
 
-    /* infinity loop -- accepting connection from client forever */
-    while(1)
-    {
-        int clientfd;
-        struct sockaddr_in client_addr;
-        int addrlen = sizeof(client_addr);
-
-        /* Wait and Accept connection */
-        clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
-
-        /* Send message */
-        send(clientfd, buffer, sizeof(buffer), 0);
-        int res=recv(clientfd, buffer1, sizeof(buffer1), 0);
-        printf("receive from client: %s, %d bytes\n", buffer1,res);
-        //write(fd, buffer1, sizeof(buffer1));
-        /* close(client) */
-        close(clientfd);
-    }
-
-    /* close(server) , but never get here because of the loop */
-    close(sockfd);
-    return 0;
+	return 0;
 }
